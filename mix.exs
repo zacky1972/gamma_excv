91dfa550b525c99cf2551db1997e274adc90d9d3
Susumu Yamazaki <zacky1972@gmail.com>
2021-11-18 19:00:56 +0900

    version 0.1.0-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GammaExcv.MixProject do
   def project do
     [
       app: :gamma_excv,
-      version: "0.1.0",
+      version: "0.1.0-dev",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
