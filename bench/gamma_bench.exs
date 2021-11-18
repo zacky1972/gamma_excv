51b0862020f13db4e90a8cf2f950afdd54ed0e31
Susumu Yamazaki <zacky1972@gmail.com>
2021-11-18 21:02:18 +0900

    create gamma_pipelined_defn (EXLA cpu)

--- bench/gamma_bench.exs
+++ bench/gamma_bench.exs
@@ -1,14 +1,27 @@
+Application.put_env(:exla, :clients,
+  default: [platform: :host],
+  cuda: [platform: :cuda]
+)
+
 {:ok, img} = Excv.imread("Pelemay.png")
 
 img
 |> GammaExcv.gamma_map(0.5)
 |> Excv.imwrite("/tmp/Pelemay_gamma.png")
 
+defmodule GammaExla do
+  import Nx.Defn
+
+  @defn_compiler EXLA
+  defn host(img, gamma), do: GammaExcv.gamma_pipelined_defn(img, gamma)
+end
+
 benches = %{
   "gamma_map (def)" => fn -> GammaExcv.gamma_map(img, 0.5) end,
   "gamma_map_pipelined (def)" => fn -> GammaExcv.gamma_map_pipelined(img, 0.5) end,
   "gamma_pipelined_def" => fn -> GammaExcv.gamma_pipelined_def(img, 0.5) end,
-  "gamma_pipelined_defn" => fn -> GammaExcv.gamma_pipelined_defn(img, 0.5) end
+  "gamma_pipelined_defn (Nx)" => fn -> GammaExcv.gamma_pipelined_defn(img, 0.5) end,
+  "gamma_pipelined_defn (EXLA cpu)" => fn -> GammaExla.host(img, 0.5) end
 }
 
 Benchee.run(
