d9e03b9876bae8bc2e9e5e95a719809c77391c79
Susumu Yamazaki <zacky1972@gmail.com>
2021-11-18 19:15:55 +0900

    create gamma_map

--- lib/gamma_excv.ex
+++ lib/gamma_excv.ex
@@ -1,5 +1,14 @@
 defmodule GammaExcv do
+  import Nx.Defn
+
   @moduledoc """
   Documentation for `GammaExcv`.
   """
+
+  @spec gamma_map(Nx.Tensor.t(), number) :: Nx.Tensor.t()
+  def gamma_map(image, gamma) do
+    image
+    |> Nx.map([type: {:f, 32}], & Nx.multiply(255, Nx.power(Nx.divide(&1, 255), Nx.divide(1, gamma))))
+    |> Nx.as_type({:u, 8})
+  end
 end
