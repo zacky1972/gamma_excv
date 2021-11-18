4000658ed6879e79d1745b21d6e66069adffc36f
Susumu Yamazaki <zacky1972@gmail.com>
2021-11-18 19:01:42 +0900

    remove hello

--- lib/gamma_excv.ex
+++ lib/gamma_excv.ex
@@ -2,17 +2,4 @@ defmodule GammaExcv do
   @moduledoc """
   Documentation for `GammaExcv`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GammaExcv.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/gamma_excv_test.exs
+++ test/gamma_excv_test.exs
@@ -1,8 +1,4 @@
 defmodule GammaExcvTest do
   use ExUnit.Case
   doctest GammaExcv
-
-  test "greets the world" do
-    assert GammaExcv.hello() == :world
-  end
 end
