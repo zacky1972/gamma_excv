f7b6ecd20b34fc719f8b8b52e3734603e4af0f1c
Susumu Yamazaki <zacky1972@gmail.com>
2021-11-18 18:58:28 +0900

    initial commit

---
+++ lib/gamma_excv.ex
@@ -0,0 +1,18 @@
+defmodule GammaExcv do
+  @moduledoc """
+  Documentation for `GammaExcv`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GammaExcv.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gamma_excv_test.exs
@@ -0,0 +1,8 @@
+defmodule GammaExcvTest do
+  use ExUnit.Case
+  doctest GammaExcv
+
+  test "greets the world" do
+    assert GammaExcv.hello() == :world
+  end
+end
