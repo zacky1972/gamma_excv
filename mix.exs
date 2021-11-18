0552d73123a04cb0155f2074d3258b1fb9c42f4e
Susumu Yamazaki <zacky1972@gmail.com>
2021-11-18 19:00:15 +0900

    install Excv

--- mix.exs
+++ mix.exs
@@ -21,8 +21,7 @@ defmodule GammaExcv.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:excv, "~> 0.1.0-dev", github: "zeam-vm/excv", branch: "main"}
     ]
   end
 end
